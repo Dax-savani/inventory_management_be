2d0dd000431a27a2a4c3469e14ec7c400999f775
Dax-savani <dakshsavani15@gmail.com>
2025-06-27 15:38:00 +0530

    service model added

---
+++ models/Service.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const serviceSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        default: ''
+    },
+    cost: {
+        type: Number,
+        required: true
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Service', serviceSchema);
